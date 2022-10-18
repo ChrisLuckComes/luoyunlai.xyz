4339a746a20841f24e6aebf373eff8b0bd57a0b2
ChrisLuckComes <313963196@qq.com>
2022-10-18 18:15:15 +0800

    新增diff

--- src/pages/vue/vue3/diff.tsx
+++ src/pages/vue/vue3/diff.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { classMap } from '@/constants/constant';
 import { Anchor } from 'antd';
-import { PATCH } from '.';
+import { PATCH, PATCH_CHILDREN, PATCH_KEYED_CHILDREN_SEQUENCE } from '.';
 const { Link } = Anchor;
 
 export default function Index() {
@@ -37,11 +37,101 @@ export default function Index() {
       <h2 id="patchChildren" className={classMap.articleTitle}>
         patchChildren
       </h2>
+      处理逻辑如下，首先获取n2的<code>patchFlag</code>
+      <ul className={classMap.ul}>
+        <li>
+          <code>patchFlag &gt; 0</code>，说明需要做diff操作，接下来关注<code>patchKeyedChildren</code>和
+          <code>patchUnkeyedChildren</code>方法
+        </li>
+        <li>
+          接下来再判断<code>shapeFlags</code>,有三种情况,text,array or no children
+          <br />
+          只有新旧节点都是ShapeFlags.ARRAY_CHILDREN才会进入diff<code>patchKeyedChildren</code>
+          ，类型不一致基本都是卸载旧节点使用新节点
+        </li>
+      </ul>
+      <div className={classMap.markdown}>{PATCH_CHILDREN}</div>
+      <br />
+      <h2 id="patchKeyedChildren" className={classMap.articleTitle}>
+        patchKeyedChildren
+      </h2>
+      接下来就是最核心的算法<code>patchKeyedChildren</code>，期间相同的节点都是直接patch
+      分步骤来看，第一步是找出相同节点
+      <h3 className={classMap.articleSubTitle}>DIFF详细步骤</h3>
+      <ul>
+        <li>
+          1.从头开始遍历，可以找出如下从头部开始的子序列
+          <br />
+          (a b) c (a b) d e
+        </li>
+        <li>
+          2. i走到e1或e2的位置从尾开始遍历，可以找到如下从尾开始的递增子序列
+          <br />a (b c) d e (b c)
+        </li>
+      </ul>
+      接着判断i和e1 e2的关系
+      <ul>
+        <li>
+          1.<code>i &gt; e1</code> 说明旧节点已遍历完，直接挂载剩余新节点
+          <br />
+          (a b) (a b) c
+        </li>
+        <li>
+          2. <code>i &gt; e2</code> 说明新节点已遍历完，卸载剩余旧节点
+          <br />
+          (a b) c (a b)
+        </li>
+        <li>
+          3. 新旧节点都没遍历完
+          <br /><br />
+          遍历剩余新节点构建<code>key:index keyToNewIndexMap</code>
+          ,新增数组 <code>newIndexToOldIndexMap</code>{' '}
+          数组长度为e2-s2+1,即未遍历完的新数组，初始值设为0，用于标记处理过的次数。
+          <br />
+          <br />
+          遍历剩余旧节点 如果<code>patched &gt;= toBePatched</code>，说明新节点已经被patch完了，剩余旧节点卸载
+          <br />
+          然后判断旧节点是否有key，则从keyToNewIndexMap中取出key对应的index;
+          <br />
+          否则只能顺序寻找是否有同类型节点，找到记录下标
+          <br /><br />
+          如果newIndex没有找到，直接卸载该节点
+          <br />
+          找到了就判断<code>newIndex &gt;=maxNewIndexSoFar</code>,记录maxNewIndexSoFar和标记moved
+          <div className={classMap.assist}>newIndex比maxIndex要小，节点肯定要移动</div>
+
+          然后patch
+          <br /><br />
+          如果moved为true，那就需要移动节点，并挂载新增节点，到了这一步就要根据newIndexToOldIndexMap获取最长递增子序列
+          <div className={classMap.assist}>
+            对于序列[1,3,2,3,5,1,66,777]的最长递增子序列是[1,2,3,5,66,777]
+          </div>
+          然后倒序遍历newIndexToOldIndexMap，每次遍历取c2[nextIndex+1]的节点作为锚点
+          <br />
+          如果newIndexToOldIndexMap[i]为0说明是新增节点，直接patch
+          只有在 <code>j &lt; 0 || i !== increasingNewIndexSequence[j]</code>才会真正移动节点
+          举个栗子
+          <br />
+          a b [c d e] f g <br />
+          a b [e d c h] f g <br />
+
+          newIndexToOldIndexMap=[4,3,2,0],increasingNewIndexSequence=[2],j=0
+          过程如下
+          <ul>
+              <li>i=3,j=0: <code>newIndexToOldIndexMap[i]===0</code> h节点是新节点 patch(h) 真实节点变为a b [c d e] h f g</li>
+              <li>i=2,j=0: <code>i === increasingNewIndexSequence[j]</code>c节点不移动，j--</li>
+              <li>i=1,j=-1: <code>j &lt; 0</code> d节点需要移动，以c为锚点move d节点，真实节点变为a b [d c e] h f g</li>
+              <li>i=0,j=-1: <code>j &lt; 0</code> e节点需要移动，以d为锚点move e节点，真实节点变为a b [e d c] h f g</li>
+          </ul>
+        </li>
+      </ul>
+      <div className={classMap.markdown}>{PATCH_KEYED_CHILDREN_SEQUENCE}</div>
       <div className="flex-between relative">
         <Anchor className="anchor" getContainer={() => document.getElementById('content') as HTMLElement}>
           <Link href="#diff" title="diff">
             <Link href="#patch" title="patch"></Link>
             <Link href="#patchChildren" title="patchChildren"></Link>
+            <Link href="#patchKeyedChildren" title="patchKeyedChildren" />
           </Link>
         </Anchor>
       </div>
--- src/pages/vue/vue3/index.ts
+++ src/pages/vue/vue3/index.ts
@@ -1420,4 +1420,244 @@ export const PATCH_CHILDREN = `  const patchChildren: PatchChildrenFn = (
   }
 }`;
 
-export const LONGEST_SUB_SEQUENCE = ``;
+export const PATCH_KEYED_CHILDREN_SEQUENCE = `  // can be all-keyed or mixed
+const patchKeyedChildren = (
+  c1: VNode[],
+  c2: VNodeArrayChildren,
+  container: RendererElement,
+  parentAnchor: RendererNode | null,
+  parentComponent: ComponentInternalInstance | null,
+  parentSuspense: SuspenseBoundary | null,
+  isSVG: boolean,
+  slotScopeIds: string[] | null,
+  optimized: boolean
+) => {
+  let i = 0
+  const l2 = c2.length
+  let e1 = c1.length - 1 // prev ending index
+  let e2 = l2 - 1 // next ending index
+
+  // 首先从头开始遍历
+  // 1. sync from start
+  // (a b) c
+  // (a b) d e
+  while (i <= e1 && i <= e2) {
+    const n1 = c1[i]
+    const n2 = (c2[i] = optimized
+      ? cloneIfMounted(c2[i] as VNode)
+      : normalizeVNode(c2[i]))
+    if (isSameVNodeType(n1, n2)) { // 如果新旧节点类型相同直接patch
+      patch(
+        n1,
+        n2,
+        container,
+        null,
+        parentComponent,
+        parentSuspense,
+        isSVG,
+        slotScopeIds,
+        optimized
+      )
+    } else { // 不同则退出循环
+      break
+    }
+    i++
+  }
+
+  // 针对性优化尾部节点可复用的情况
+  // 2. sync from end
+  // a (b c)
+  // d e (b c)
+  while (i <= e1 && i <= e2) {
+    const n1 = c1[e1]
+    const n2 = (c2[e2] = optimized
+      ? cloneIfMounted(c2[e2] as VNode)
+      : normalizeVNode(c2[e2]))
+    if (isSameVNodeType(n1, n2)) {
+      patch(
+        n1,
+        n2,
+        container,
+        null,
+        parentComponent,
+        parentSuspense,
+        isSVG,
+        slotScopeIds,
+        optimized
+      )
+    } else {
+      break
+    }
+    e1--
+    e2--
+  }
+  // 3. common sequence + mount 说明旧节点已遍历完，直接mount剩余新节点
+  // (a b)
+  // (a b) c
+  // i = 2, e1 = 1, e2 = 2
+  // (a b)
+  // c (a b)
+  // i = 0, e1 = -1, e2 = 0
+  if (i > e1) {
+    if (i <= e2) {
+      const nextPos = e2 + 1
+      const anchor = nextPos < l2 ? (c2[nextPos] as VNode).el : parentAnchor
+      while (i <= e2) {
+        patch(
+          null,
+          (c2[i] = optimized
+            ? cloneIfMounted(c2[i] as VNode)
+            : normalizeVNode(c2[i])),
+          container,
+          anchor,
+          parentComponent,
+          parentSuspense,
+          isSVG,
+          slotScopeIds,
+          optimized
+        )
+        i++
+      }
+    }
+  }
+
+  // 4. common sequence + unmount 说明新节点已遍历完，卸载剩余旧节点
+  // (a b) c
+  // (a b)
+  // i = 2, e1 = 2, e2 = 1
+  // a (b c)
+  // (b c)
+  // i = 0, e1 = 0, e2 = -1
+  else if (i > e2) {
+    while (i <= e1) {
+      unmount(c1[i], parentComponent, parentSuspense, true)
+      i++
+    }
+  }
+
+  // 5. unknown sequence 
+  // [i ... e1 + 1]: a b [c d e] f g
+  // [i ... e2 + 1]: a b [e d c h] f g
+  // i = 2, e1 = 4, e2 = 5
+  else {
+    const s1 = i // prev starting index
+    const s2 = i // next starting index
+    // 首先遍历剩余新节点组成map，key不变，value为下标
+    // 5.1 build key:index map for newChildren
+    const keyToNewIndexMap: Map<string | number | symbol, number> = new Map()
+    for (i = s2; i <= e2; i++) {
+      const nextChild = (c2[i] = optimized
+        ? cloneIfMounted(c2[i] as VNode)
+        : normalizeVNode(c2[i]))
+      if (nextChild.key != null) { // key不能为空
+        if (__DEV__ && keyToNewIndexMap.has(nextChild.key)) {
+          warn(
+            'Duplicate keys found during update:',
+            JSON.stringify(nextChild.key),
+            'Make sure keys are unique.'
+          )
+        }
+        keyToNewIndexMap.set(nextChild.key, i)
+      }
+    }
+    // 遍历剩余旧节点
+    // 5.2 loop through old children left to be patched and try to patch
+    // matching nodes & remove nodes that are no longer present
+    let j
+    let patched = 0
+    const toBePatched = e2 - s2 + 1
+    let moved = false
+    // used to track whether any node has moved
+    let maxNewIndexSoFar = 0
+    // works as Map<newIndex, oldIndex>
+    // Note that oldIndex is offset by +1
+    // and oldIndex = 0 is a special value indicating the new node has
+    // no corresponding old node.
+    // used for determining longest stable subsequence
+    const newIndexToOldIndexMap = new Array(toBePatched)
+    for (i = 0; i < toBePatched; i++) newIndexToOldIndexMap[i] = 0
+
+    for (i = s1; i <= e1; i++) {
+      const prevChild = c1[i]
+      if (patched >= toBePatched) {
+        // all new children have been patched so this can only be a removal
+        unmount(prevChild, parentComponent, parentSuspense, true)
+        continue
+      }
+      let newIndex
+      if (prevChild.key != null) {
+        newIndex = keyToNewIndexMap.get(prevChild.key)
+      } else {
+        // key-less node, try to locate a key-less node of the same type
+        for (j = s2; j <= e2; j++) {
+          if (
+            newIndexToOldIndexMap[j - s2] === 0 &&
+            isSameVNodeType(prevChild, c2[j] as VNode)
+          ) {
+            newIndex = j
+            break
+          }
+        }
+      }
+      if (newIndex === undefined) {
+        unmount(prevChild, parentComponent, parentSuspense, true)
+      } else {
+        newIndexToOldIndexMap[newIndex - s2] = i + 1
+        if (newIndex >= maxNewIndexSoFar) {
+          maxNewIndexSoFar = newIndex
+        } else {
+          moved = true
+        }
+        patch(
+          prevChild,
+          c2[newIndex] as VNode,
+          container,
+          null,
+          parentComponent,
+          parentSuspense,
+          isSVG,
+          slotScopeIds,
+          optimized
+        )
+        patched++
+      }
+    }
+
+    // 5.3 move and mount
+    // generate longest stable subsequence only when nodes have moved
+    const increasingNewIndexSequence = moved
+      ? getSequence(newIndexToOldIndexMap)
+      : EMPTY_ARR
+    j = increasingNewIndexSequence.length - 1
+    // looping backwards so that we can use last patched node as anchor
+    for (i = toBePatched - 1; i >= 0; i--) {
+      const nextIndex = s2 + i
+      const nextChild = c2[nextIndex] as VNode
+      const anchor =
+        nextIndex + 1 < l2 ? (c2[nextIndex + 1] as VNode).el : parentAnchor
+      if (newIndexToOldIndexMap[i] === 0) {
+        // mount new
+        patch(
+          null,
+          nextChild,
+          container,
+          anchor,
+          parentComponent,
+          parentSuspense,
+          isSVG,
+          slotScopeIds,
+          optimized
+        )
+      } else if (moved) {
+        // move if:
+        // There is no stable subsequence (e.g. a reverse)
+        // OR current node is not among the stable sequence
+        if (j < 0 || i !== increasingNewIndexSequence[j]) {
+          move(nextChild, container, anchor, MoveType.REORDER)
+        } else {
+          j--
+        }
+      }
+    }
+  }
+}`;
